af538e4a8c5b7ca65431e0d1f611bd4660058501
gcuellar <german.cuellar.jimenez@gmail.com>
2021-02-15 21:04:26 +0100

    Create Car.java

---
+++ neo1/Car.java
@@ -0,0 +1,40 @@
+public class Car{
+    private String license;
+    private String model;
+
+    public Car(){
+    }
+
+    public Car(String license, String model){
+        this.license = license;
+        this.model = model;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Car && ((Car)obj).toString().equals(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "license='" + license + '\'' +
+                '}';
+    }
+}
